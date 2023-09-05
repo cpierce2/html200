ec3d38bd73c0d1b182add03891133cc0d38d3671
Christopher Pierce <c.pierce2@pm.me>
2023-09-04 19:28:02 -0700

    modified all files with code

--- l4c1.js
+++ l4c1.js
@@ -1,9 +1,9 @@
 function numberSquare() {
     let input = prompt('Enter a number');
-    let numberToSquare = 2;
+    let numberToSquare = input;
     let result = 0;
   
-    result = numberToSquare + Number(input);
+    result = numberToSquare * Number(input);
     
     alert(result);
 }
